957e6fcdeb39fa397476485f59fb2626d6976bd2
gilianfavour <gilianfavour06gmail.com>
2026-05-12 10:24:05 +0300

    edited config files

--- secure-talk-api/config/cors.php
+++ secure-talk-api/config/cors.php
@@ -3,10 +3,10 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'], // or your frontend URL
+    'allowed_origins' => ['https://secure-talk-jbjm.onrender.com'], // or your frontend URL
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 ];
